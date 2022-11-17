7aac36bcc2470615dbd4291664f409ec030811e0
Phat Bui <phat.bui@southasiaservices.com>
2022-11-17 15:38:53 +0700

    update script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+git diff ./ios/cicd/Info.plist
 git remote add origin https://PttB422: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@github.com/PttB422/cicd.git
 git checkout ${APP_ENV}
 git add .
