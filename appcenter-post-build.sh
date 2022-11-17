2d41ad22a29526e27d492698927078f2058a769a
Phat Bui <phat.bui@southasiaservices.com>
2022-11-17 15:03:50 +0700

    update script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+git remote add origin https://github.com/PttB422/cicd.git
 git checkout ${APP_ENV}
 git add .
 git commit -am "build and bump version"
-git push
+git push origin ${APP_ENV}
