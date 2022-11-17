b73f6ed42f669207d8752fc3212d517f15653a52
Phat Bui <phat.bui@southasiaservices.com>
2022-11-17 16:25:14 +0700

    remove git pw

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git diff ./ios/cicd/Info.plist
-git remote add origin https://PttB422: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@github.com/PttB422/cicd.git
+git remote add origin https://PttB422:${GIT_PW}@github.com/PttB422/cicd.git
 git checkout ${APP_ENV}
 git add .
 git commit -am "build and bump version"
