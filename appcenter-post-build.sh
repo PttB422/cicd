9016594eeb900aac3c23bbad99db684b221e149f
Phat Bui <phat.bui@southasiaservices.com>
2022-11-17 11:23:26 +0700

    update script

---
+++ appcenter-post-build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git add .
+git commit -am "build and bump version"
+git push
---
+++ appcenter-post-clone.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "APP_ENV=${APP_ENV}" > ".env"
+echo "APP_VERSION=${APP_VERSION}" >> ".env"
